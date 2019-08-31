ffe447899e2b1f47b0d3e8eb1440d4128d8e7073
kevin lang <langkevina@gmail.com>
2019-08-31 22:54:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+      erb :index
+    end
+
+    post '//puppy' do
+      @analyzed_text = TextAnalyzer.new(params[:user_text])
+
+      erb :results
+    end
 
 end
